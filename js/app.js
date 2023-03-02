d0191cdd920b6ba1e0da944560ae9a19311e3838
tuhin <tuhin153695@gmail.com>
2023-03-02 22:08:17 +0600

    phone hunter done

---
+++ js/app.js
@@ -0,0 +1,128 @@
+const loadPhone = async(searchText, dataLimit) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data, dataLimit);
+}
+
+
+
+const displayPhones = (phones, dataLimit) =>{
+    // console.log(phones);
+    const phonesContainer = document.getElementById('phones-container');
+    // clear previous item & show only current search item
+    phonesContainer.textContent = '';
+
+    // display 10 phones
+    const showAll = document.getElementById('show-all');
+
+    if(dataLimit && phones.length > 10){
+        phones = phones.slice(0,10);
+        showAll.classList.remove('d-none');
+    }
+    else{
+        showAll.classList.add('d-none');
+    }
+
+    // display no phone found
+    const noPhone = document.getElementById('no-found-message');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none');
+    }
+    else{
+        noPhone.classList.add('d-none');
+    }
+
+
+
+
+    // display all phones
+    phones.forEach(phone =>{
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML =`
+        <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top p-4" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
+            </div>
+        </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
+    });
+    // stop loader
+    toggleSpinner(false);
+}
+
+const processSearch = (dataLimit) =>{
+    toggleSpinner(true);
+    const searchField = document.getElementById('searchField');
+    const searchText = searchField.value;
+    loadPhone(searchText, dataLimit);
+}
+
+// handle search button click
+document.getElementById('btn-search').addEventListener('click', function(){
+    // start loader
+    toggleSpinner(true);
+    const searchField = document.getElementById('searchField');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+    processSearch(10);
+
+})
+
+//search input field enter key handler
+document.getElementById('searchField').addEventListener('keypress', function (e) {
+    console.log(e.key);
+    if (e.key === 'Enter') {
+        processSearch(10);
+    }
+});
+
+
+
+
+
+const toggleSpinner = isLoading =>{
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none')
+    }
+}
+
+//not the best way to load show All
+document.getElementById('btn-show-all').addEventListener('click', function(){
+ processSearch();
+})
+
+
+// details
+const loadPhoneDetails = async id =>{
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone =>{
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    // <img  class="img-fluid rounded mb-4" src="${phone.image}"/>
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.innerHTML = `
+    <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'}</p>
+    <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No Storage Information '}</p>
+    <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Information'}</p>
+    <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
+`
+}
+
+
+loadPhone('apple');
